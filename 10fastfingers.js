1bb70cb8495ed24adbf799daa7c77510f7d79826
naveenarun <notatroll.troll@gmail.com>
2016-10-18 22:48:08 -0700

    fix typo

--- 10fastfingers.js
+++ 10fastfingers.js
@@ -1,7 +1,7 @@
 // For http://10fastfingers.com/typing-test/english
 
 // Fast Version - Rank 1 Worldwide (10/18/2016)
-// 156 Charactesr
+// 156 Characters
 x=Array.prototype.slice.call($("[wordnr]"));countdown=0;x.map(function(x){$inputfield.val(x.innerHTML); $inputfield.trigger({type: 'keyup', which: 32});});
 
 // ES6 "Tweetable" Version - Rank 1 Worldwide (10/18/2016)
