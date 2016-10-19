a607db35fbf0b8f294f367b90ef981c8f9281475
naveenarun <notatroll.troll@gmail.com>
2016-10-18 22:41:46 -0700

    fix error in ES6 version

--- 10fastfingers.js
+++ 10fastfingers.js
@@ -1,7 +1,9 @@
-// Fast Version - Rank 1 Worldwide (10/16/2016)
+// For http://10fastfingers.com/typing-test/english
+
+// Fast Version - Rank 1 Worldwide (10/18/2016)
+// 156 Charactesr
 x=Array.prototype.slice.call($("[wordnr]"));countdown=0;x.map(function(x){$inputfield.val(x.innerHTML); $inputfield.trigger({type: 'keyup', which: 32});});
 
-// ES6 "Tweetable" Version - Rank 14 Worldwide (10/18/2016)
-x.map(function(x){$inputfield.val(x.innerHTML); $inputfield.trigger({type: 'keyup', which: 32});});
-
-
+// ES6 "Tweetable" Version - Rank 1 Worldwide (10/18/2016)
+// 108 Characters
+i=$inputfield;countdown=0;$.map($("[wordnr]"),x=>{i.val(x.innerHTML);i.trigger({type:'keyup',which:32});});
